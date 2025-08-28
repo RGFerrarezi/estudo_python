c37d455cb5718a6399d3478c5fbb562eed4219da
RGFerrarezi <robson.ferrarezi.st@prometeon.com>
2025-08-28 08:58:27 -0300

    API completa com CRUD fucionando, e salvando informações em banco de dados

--- restAPI/main.py
+++ restAPI/main.py
@@ -1,23 +1,61 @@
 from fastapi import FastAPI, HTTPException
-from models import User, create_db_and_tables, engine, Session, select
+from models import User, UserRead, UserCreate, create_db_and_tables, engine, Session, select, constr, conint, EmailStr
 
 app = FastAPI()
 
 create_db_and_tables()
 
-@app.post("/users/", response_model=User)
-def create_user(user: User):
+@app.post("/users/", response_model=UserRead)
+def create_user(user: UserCreate):
     with Session(engine) as session:
+        db_user = User.model_validate(user)
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+        return db_user
+    
+#Read, single, procura por ID e retorna o usuário se houver
+@app.get("/users/{user_id}", response_model=UserRead)
+def read_user(user_id: int):
+    with Session(engine) as session:
+        statement = select(User).where(User.id == user_id)
+        result = session.exec(statement).first()
+        if result is None:
+            raise HTTPException(status_code=404, detail="Usuário não encontrado!")
+        return result
+    
+#Read, all, retorna todos os usuários
+@app.get("/users/", response_model=list[UserRead])
+def read_users():
+    with Session(engine) as session:
+        statement = select(User)
+        results = session.exec(statement).all()
+        return results
+    
+#Update
+@app.put("/users/{user_id}", response_model=UserRead)
+def update_user(user_id: int, updated_user: UserCreate):
+    with Session(engine) as session:
+        statement = select(User).where(User.id == user_id)
+        user = session.exec(statement).first()
+        if user is None:
+            raise HTTPException(status_code=404, detail="Usuário não encontrado!")
+        user.name = updated_user.name
+        user.email = updated_user.email
+        user.age + updated_user.age
         session.add(user)
         session.commit()
         session.refresh(user)
         return user
     
-@app.get("/users/{user_id}", response_model=User)
-def read_user(user_id: int):
+#Delete
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int):
     with Session(engine) as session:
         statement = select(User).where(User.id == user_id)
-        results = session.exec(statement).first()
-        if results is None:
+        user = session.exec(statement).first()
+        if user is None:
             raise HTTPException(status_code=404, detail="Usuário não encontrado!")
-        return results
+        session.delete(user)
+        session.commit()
+        return{"ok": True, "message": "Usuário deletado com sucesso!"}
--- restAPI/models.py
+++ restAPI/models.py
@@ -1,13 +1,30 @@
 from sqlmodel import SQLModel, Field, create_engine, Session, select
+from pydantic import EmailStr, constr, conint
 
+#Modelo principal
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
     email: str
     age: int
 
+#Modelo para entrada de dados
+class UserCreate(SQLModel):
+    name: constr(min_length=1) # garantindo que o nome não seja vazio
+    email: EmailStr # garantindo que o email seja válido
+    age: conint(ge=0) # garantindo que a idade esteja entre 1 e 119
+
+#Modelo para saída de dados:
+class UserRead(SQLModel):
+    id: int
+    name: str
+    email: str
+    age: int
+
+#Configuração do banco de dados
 sqlite_file_name = "users.db"
 engine = create_engine(f"sqlite:///{sqlite_file_name}",echo=True)
 
+#Criar o banco de dados e as tabelas
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
